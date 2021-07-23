1c7c09e4f38f5d8f92fc84483146290371ddb0a2
Aashish Aryal <39742665+aryalaashish121@users.noreply.github.com>
2021-07-23 08:31:46 +0545

    top nav icon setup

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <link href="images/saarc_logo.jpg" rel="icon">
     <title>@yield('title')</title>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <!-- Fonts -->
